8d9896314b1dd26d8dc9f140313b10bdfb2ca907
raghav-anand <rsanand2@illinois.edu>
2022-03-10 19:18:09 -0600

    fixed scripts

--- csv_scripts/association.py
+++ csv_scripts/association.py
@@ -3,14 +3,15 @@ df = pd.read_csv("MoviesOnStreamingPlatforms.csv")
 ret = ""
 for i in range(len(df)):
     if df['Netflix'][i] == 1:
-        ret += "INSERT INTO MoviePlatformAssociation VALUES(" + str(df['ID'][i]) + ", 'Netflix')\n"
+        ret += "INSERT INTO MoviePlatformAssociation VALUES(" + str(df['ID'][i]) + ", 'Netflix');\n"
         
     if df['Hulu'][i] == 1:
-        ret += "INSERT INTO MoviePlatformAssociation VALUES(" + str(df['ID'][i]) + ", 'Hulu')\n"
+        ret += "INSERT INTO MoviePlatformAssociation VALUES(" + str(df['ID'][i]) + ", 'Hulu');\n"
     if df['Prime Video'][i] == 1:
-        ret += "INSERT INTO MoviePlatformAssociation VALUES(" + str(df['ID'][i]) + ", 'Prime Video')\n"
+        ret += "INSERT INTO MoviePlatformAssociation VALUES(" + str(df['ID'][i]) + ", 'Prime Video');\n"
     if df['Disney+'][i] == 1:
-        ret += "INSERT INTO MoviePlatformAssociation VALUES(" + str(df['ID'][i]) + ", 'Disney+')\n"
+        ret += "INSERT INTO MoviePlatformAssociation VALUES(" + str(df['ID'][i]) + ", 'Disney+');\n"
+    
 
 f = open("association.sql", "w+")
 f.write(ret)
