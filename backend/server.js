46c4b72054c6f39a8abfdffe0b9ca2799d809e97
raghav-anand <rsanand2@illinois.edu>
2022-04-03 17:09:51 -0500

    added platform query

--- backend/server.js
+++ backend/server.js
@@ -122,3 +122,13 @@ app.post('/listmovie', (request, response) => {
     });
 });
 
+app.get('/platforms', (request, response) => {
+    var movieId = request.query.movieId;
+    var sql = `SELECT a.PlatformName FROM MoviePlatformAssociation a WHERE a.MovieId = ${movieId}`;
+    connection.query(sql, (err, result) => {
+        if (err) {
+            response.status(400).send('Error in database operation');
+        }
+        response.send(result);
+    })
+});
