40774af76c1daa9df862ca3d862548bf38bb612d
raghav-anand <rsanand2@illinois.edu>
2022-04-04 00:48:35 -0500

    fixed platforms

--- backend/server.js
+++ backend/server.js
@@ -141,15 +141,16 @@ app.get('/movies', (request, response) => {
 // });
 
 app.get('/platforms', (request, response) => {
-    var movieId = request.query.movieId;
-    var sql = `SELECT a.PlatformName FROM MoviePlatformAssociation a WHERE a.MovieId = ${movieId}`;
+    var sql = "SELECT * FROM MoviePlatformAssociation a"
     console.log(sql);
+
     connection.query(sql, (err, result) => {
         if (err) {
             response.status(400).send('Error in database operation');
         }
         response.send(result);
-    })
+    });
+    
 });
 
 // app.put('/ratings', (request, response) => {
