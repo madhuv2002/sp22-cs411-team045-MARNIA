43f9c3e52b0d4f4b2525d065fe0a9cfff8b5ea9c
konjeti3 <konjeti3@illinois.edu>
2022-04-27 19:28:50 -0500

    Login css

--- flixfind/src/pages/login.js
+++ flixfind/src/pages/login.js
@@ -2,6 +2,7 @@ import { Form, Input, Button, Checkbox } from 'antd';
 import './Title.css';
 import '@ant-design/compatible/assets/index.css';
 import 'antd/dist/antd.css'; 
+import './login.css';
 
 const LoginPage = () => {
   const onFinish = (values) => {
@@ -17,6 +18,7 @@ const LoginPage = () => {
     <div className="Title">
         <h1>FlixFind+</h1>
       </div>
+      <div className='title-acc'><h2>Existing Users</h2></div>
     <Form
       name="basic"
       labelCol={{
@@ -58,16 +60,73 @@ const LoginPage = () => {
         <Input.Password />
       </Form.Item>
 
+
       <Form.Item
-        name="remember"
-        valuePropName="checked"
         wrapperCol={{
           offset: 8,
           span: 16,
         }}
       >
-        <Checkbox>Remember me</Checkbox>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
       </Form.Item>
+    </Form>
+    <div className='title-acc'><h2>Create An Account</h2></div>
+    <Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your username!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+      <Form.Item
+        label="Age"
+        name="age"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your age!',
+          },
+        ]}
+      >
+        <Input/>
+      </Form.Item>
+
+ 
 
       <Form.Item
         wrapperCol={{
