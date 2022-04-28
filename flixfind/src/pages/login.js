ee81aeb8232dc1b764ab9b1ce1633c89b4f8b5c4
konjeti3 <konjeti3@illinois.edu>
2022-04-27 19:15:43 -0500

    Login starter code

--- flixfind/src/pages/login.js
+++ flixfind/src/pages/login.js
@@ -1,18 +1,79 @@
-import ReactDOM from 'react-dom';
-import React, { useEffect } from 'react';
-import '@ant-design/compatible/assets/index.css';
-import 'antd/dist/antd.css'; 
-import './Title.css';
+import { Form, Input, Button, Checkbox } from 'antd';
 
-function LoginPage() {
+const LoginPage = () => {
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
   return (
-    <div className="App">
-      <div className="Title">
-        <h1>LOGIN+</h1>
-      </div>
-     
-    </div>
+    <Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your username!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name="remember"
+        valuePropName="checked"
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
   );
-}
+};
 
 export default LoginPage;
